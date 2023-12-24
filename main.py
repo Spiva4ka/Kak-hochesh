77c8de22869819c9e2b36c6d33d42b5577f0b9a0
Your Name <you@example.com>
2023-12-24 14:03:43 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,17 @@
+class Unit:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+class Soldier(Unit):
+    def __init__(self, name, health, attack_power):
+        super().__init__(name, health)
+        self.attack_power = attack_power
+    def attack(self, enemy):
+        enemy -= self.attack_power
+class Archer(Unit):
+    def __init__(self, name, health, range_attack_power):
+        super().__init__(name, health)
+        self.range_attack_power = range_attack_power
+    def attack(self, enemy):
+        enemy -= self.range_attack_power
